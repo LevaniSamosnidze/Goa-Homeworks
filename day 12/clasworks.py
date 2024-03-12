c4cc7df9425812cfd0d38508c38f95aeef6d37da
LevaniSamosnidze <levanisamsonidze9@gmail.com>
2024-03-12 21:53:10 +0400

    commit

--- day 12/clasworks.py
+++ day 12/clasworks.py
@@ -1,14 +1,11 @@
 score1 = int(input("please enter your sqore " ))
-if score1 > 90 :
- if score1 < 100:
-  print("you will be financed")
-if score1 > 70:
- if score1 < 80:
-  print("You have financed your studies with 1500 GEL")
-  if score1 > 40:
-    if score1 < 70:
-     print("You have financed your studies with 500 GEL")
-if score1 < 40:
+if 90 <  score1 < 100:
+ print("you will be financed")
+elif 70< score1 <80:
+ print("You have financed your studies with 1500 GEL")
+elif 40< score1 <70:
+ print("You have financed your studies with 500 GEL")
+elif score1 < 40:
  print("You did not finance your studies")
 
 
---
+++ day 12/homworks.py
@@ -0,0 +1,9 @@
+student = int(input("enter your sing "))
+if student == 10:
+    print("parent i is cild good student ")
+elif student == 9 or 9:
+    print("your cild the  good students  He misses a few lessons")
+elif student == 5:
+    print("Your child does not attend lessons ")
+elif student < 5:
+    print("your cild kick off shcool")
